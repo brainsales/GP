1b23a59c701b5259e54a97b9b98c0d15ea2aaa44
Ryu <brainsales@naver.com>
2017-05-26 17:32:24 +0900

    add splash

---
+++ app/src/main/java/com/brainsales/gameport/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.brainsales.gameport;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by user on 2017-05-26.
+ */
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+
+
+    }
+
+}
