65656362fe8b1ef1c0b378be702ae2c1b69e81ea
Ryu <brainsales@naver.com>
2017-06-06 14:22:53 +0900

    add Image

--- app/src/main/java/com/brainsales/gameport/PostingActivity.java
+++ app/src/main/java/com/brainsales/gameport/PostingActivity.java
@@ -1,13 +1,45 @@
 package com.brainsales.gameport;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostingActivity extends AppCompatActivity {
+
+    private ImageButton mSelectImage;
+
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting);
+
+        mSelectImage = (ImageButton) findViewById(R.id.imageButton);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+
+            Uri imageUri = data.getData();
+            mSelectImage.setImageURI(imageUri);
+
+        }
     }
 }
